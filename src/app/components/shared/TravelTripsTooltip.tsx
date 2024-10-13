a5d12a75cb34a120f5017bff79919f136606c02e
ronyislam502 <ronyislam502@gmail.com>
2024-10-13 20:49:44 +0600

    shared done

---
+++ src/app/components/shared/TravelTripsTooltip.tsx
@@ -0,0 +1,24 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import React from "react";
+
+interface IProps {
+  children: React.ReactNode;
+  message: React.ReactNode;
+}
+
+const TravelTripsTooltip: React.FC<IProps> = ({ children, message }) => {
+  return (
+    <TooltipProvider delayDuration={200}>
+      <Tooltip>
+        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipContent>{message}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+export default TravelTripsTooltip;
